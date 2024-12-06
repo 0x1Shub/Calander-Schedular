c7b45f940763f56592c1d5d3be5849e7bc736762
0x1Shub <shubhamyeram13@gmail.com>
2024-12-06 19:54:37 +0530

    Implemented username & fullname

--- app/components/SubmitButtons.tsx
+++ app/components/SubmitButtons.tsx
@@ -6,6 +6,29 @@ import GoogleLogo from '@/public/google.svg';
 import GithubLogo from '@/public/github.svg';
 import Image from "next/image";
 import { Loader2 } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+interface SubmitButtonProps {
+    text: string;
+    variant?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link" | null | undefined;
+    className?: string; 
+}
+
+export function SubmitButton({text, variant, className} : SubmitButtonProps){
+    const {pending} = useFormStatus()
+
+    return (
+        <>
+            {pending ? (
+                <Button disabled variant={'outline'} className={cn('w-fit', className)}>
+                    <Loader2 className="size-4 mr-2 animate-spin" /> Please wait
+                </Button>
+            ) : (
+                <Button type="submit" variant={variant} className={cn('w-fit', className)}>{text}</Button>
+            )}
+        </>
+    )
+}
 
 export function GoogleAuthButton(){
     const { pending } = useFormStatus()
