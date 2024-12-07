a9ba76834e98c7bc24a581b129ba9ce20295eb8e
0x1Shub <shubhamyeram13@gmail.com>
2024-12-07 20:44:32 +0530

    Implemented grant-id route

---
+++ app/onboarding/grant-id/page.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import VideoGif from '@/public/work-is-almost-over-happy.gif';
+import Image from "next/image";
+import Link from "next/link";
+import { CalendarCheck2 } from 'lucide-react';
+
+export default function OnboardingRouteTwo(){
+    return (
+       <div className="min-h-screen w-screen flex items-center justify-center">
+            <Card>
+                <CardHeader>
+                    <CardTitle>You are almost Done!</CardTitle>
+                    <CardDescription>We have to now connect your calander to your account</CardDescription>
+                    <Image src={VideoGif} alt="Almost finised gif" className="w-full rounded-lg"  />
+                </CardHeader>
+                <CardContent>
+                    <Button asChild className="w-full">
+                        <Link href={'/'}>
+                            <CalendarCheck2 className="size-4 mr-2" />
+                            Connect Calander to your Account
+                        </Link>
+                    </Button>
+                </CardContent>
+            </Card>
+       </div>
+    )
+}   
