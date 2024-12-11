efa70a90914ed6b81dc4618dbab4f92932e7673f
0x1Shub <shubhamyeram13@gmail.com>
2024-12-11 12:04:52 +0530

    Implemented Event Types route

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,47 @@
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 import { auth } from "../lib/auth"
 import { requireUser } from "../lib/hooks";
+import prisma from "../lib/db";
+import { EmptyState } from "../components/EmptyState";
+
+async function getData(userId: string) {
+    const data = await prisma.user.findUnique({
+        where: {
+            id: userId,
+        },
+        select: {
+            userName: true,
+            eventType: {
+                select: {
+                    id: true,
+                    active: true,
+                    title: true,
+                    url: true,
+                    duration: true,
+                }
+            }
+        }
+    });
+
+    if(!data){
+        return notFound();
+    }
+
+    return data;
+}
 
 export default async function DashboardPage(){
 
     const session = await requireUser();
+    const data = await getData(session.user?.id as string) ;
 
     return (
-        <div>
-            <h1>Hello from dashboard</h1>
-        </div>
+        <>
+            {data.eventType.length === 0 ? (
+                <EmptyState title="You have no  Event Types" description="You can create your first event type by clicking the button below" buttonText="Add event type" href="/dashboard/new" />
+            ) : (
+                <p>hey we have event types</p>
+            )}
+        </>
     )
-}
+}   
