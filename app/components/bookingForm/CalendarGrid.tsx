7162582fe2868174989aecf1eff8d4cdb946cef5
0x1Shub <shubhamyeram13@gmail.com>
2024-12-17 20:36:11 +0530

    Implement Calendar Grid

--- app/components/bookingForm/CalendarGrid.tsx
+++ app/components/bookingForm/CalendarGrid.tsx
@@ -1,45 +1,51 @@
-import {useCalendarGrid, useLocale} from 'react-aria';
-import {getWeeksInMonth, DateDuration, endOfMonth} from '@internationalized/date';
-import { CalendarState } from 'react-stately';
-import { CalendarProps, DateValue } from "@react-types/calendar";
-import { CalendarCell } from './CalendarCell';
+import { useCalendarGrid, useLocale, DateValue } from "react-aria";
+import {
+  getWeeksInMonth,
+  DateDuration,
+  endOfMonth,
+} from "@internationalized/date";
+import { CalendarState } from "react-stately";
+import { CalendarCell } from "./CalendarCell";
 
 export function CalendarGrid({
   state,
   offset = {},
   isDateUnavailable,
-} : {
-  state: CalendarState; 
+}: {
+  state: CalendarState;
   offset?: DateDuration;
   isDateUnavailable?: (date: DateValue) => boolean;
 }) {
-
   const startDate = state.visibleRange.start.add(offset);
-  const endDate = endOfMonth(startDate);
 
+  const endDate = endOfMonth(startDate);
   let { locale } = useLocale();
-  let { gridProps, headerProps, weekDays } = useCalendarGrid({
-    startDate,
-    endDate,
-    weekdayStyle: 'short'
-  }, state);
+  let { gridProps, headerProps, weekDays } = useCalendarGrid(
+    {
+      startDate,
+      endDate,
+      weekdayStyle: "short",
+    },
+    state
+  );
 
   const weeksInMonth = getWeeksInMonth(startDate, locale);
-
-
   return (
-    <table {...gridProps} cellPadding={0} className='flex-1'>
-      <thead {...headerProps} className='text-sm font-medium'> 
+    <table {...gridProps} cellPadding={0} className="flex-1">
+      <thead {...headerProps} className="text-sm font-medium">
         <tr>
-          {weekDays.map((day, index) => <th key={index}>{day}</th>)}
+          {weekDays.map((day, index) => (
+            <th key={index}>{day}</th>
+          ))}
         </tr>
       </thead>
       <tbody>
         {[...new Array(weeksInMonth).keys()].map((weekIndex) => (
           <tr key={weekIndex}>
-            {state.getDatesInWeek(weekIndex).map((date, i) => (
-              date
-                ? (
+            {state
+              .getDatesInWeek(weekIndex)
+              .map((date, i) =>
+                date ? (
                   <CalendarCell
                     currentMonth={startDate}
                     key={i}
@@ -47,9 +53,10 @@ export function CalendarGrid({
                     date={date}
                     isUnavailable={isDateUnavailable?.(date)}
                   />
+                ) : (
+                  <td key={i} />
                 )
-                : <td key={i} />
-            ))}
+              )}
           </tr>
         ))}
       </tbody>
