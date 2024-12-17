bf6a9b08b16b183dde1da9ef63ebabdf5ed0319c
0x1Shub <shubhamyeram13@gmail.com>
2024-12-17 20:23:58 +0530

    Implemented Calendar Cell

--- app/components/bookingForm/CalendarCell.tsx
+++ app/components/bookingForm/CalendarCell.tsx
@@ -1,9 +1,13 @@
 import { useRef } from "react";
-import {useCalendarCell, useFocusRing, mergeProps} from 'react-aria';
-import { CalendarState } from 'react-stately';
-import {CalendarDate, isToday, getLocalTimeZone, isSameMonth} from '@internationalized/date';
+import { useCalendarCell, useFocusRing, mergeProps } from "react-aria";
+import { CalendarState } from "react-stately";
+import {
+  CalendarDate,
+  isToday,
+  getLocalTimeZone,
+  isSameMonth,
+} from "@internationalized/date";
 import { cn } from "@/lib/utils";
-import { CalendarProps, DateValue } from "@react-types/calendar";
 
 export function CalendarCell({
   state,
@@ -11,47 +15,55 @@ export function CalendarCell({
   currentMonth,
   isUnavailable,
 }: {
-  state: CalendarState, 
-  date: CalendarDate, 
-  currentMonth: CalendarDate,
+  state: CalendarState;
+  date: CalendarDate;
+  currentMonth: CalendarDate;
   isUnavailable?: boolean;
-}){
-
+}) {
   let ref = useRef(null);
-  let {
-    cellProps,
-    buttonProps,
-    isSelected,
-    isDisabled,
-    formattedDate
-  } = useCalendarCell({ date }, state, ref);
+  let { cellProps, buttonProps, isSelected, isDisabled, formattedDate } =
+    useCalendarCell({ date }, state, ref);
 
   const { focusProps, isFocusVisible } = useFocusRing();
 
-  const isDateToday = isToday(date,getLocalTimeZone());
+  const isDateToday = isToday(date, getLocalTimeZone());
   const isOutsideOfMonth = !isSameMonth(currentMonth, date);
 
   const finallyIsDisabled = isDisabled || isUnavailable;
 
-  return(
-   <td {...cellProps} className={`py-0.5 px-0.5 relative ${isFocusVisible ? 'z-10': 'z-0'}`}>
-    <div 
-      {...mergeProps(buttonProps, focusProps)} 
-      ref={ref} 
-      hidden={isOutsideOfMonth}
-      className="size-10 sm:size-12 outline-none group rounded-md"
+  return (
+    <td
+      {...cellProps}
+      className={`py-0.5 px-0.5 relative ${isFocusVisible ? "z-10" : "z-0"}`}
     >
-      <div className={cn("size-4 rounded-sm flex items-center justify-center text-sm font-semibold", 
-        isSelected ? 'bg-primary text-white': '',
-        finallyIsDisabled ? 'text-muted-foreground cursor-not-allowed': "",
-        !isSelected && !finallyIsDisabled ? "bg-secondary" : "")}>
+      <div
+        {...mergeProps(buttonProps, focusProps)}
+        ref={ref}
+        hidden={isOutsideOfMonth}
+        className="size-10 sm:size-12 outline-none group rounded-md"
+      >
+        <div
+          className={cn(
+            "size-full rounded-sm flex items-center justify-center text-sm font-semibold",
+
+            finallyIsDisabled ? "text-muted-foreground cursor-not-allowed" : "",
+
+            isSelected ? "bg-primary text-white" : "",
+
+            !isSelected && !finallyIsDisabled ? "bg-secondary" : ""
+          )}
+        >
           {formattedDate}
           {isDateToday && (
-            <div className={cn('absolute bottom-3 left-1/2 transform -translate-x-1/2 translate-y-1/2 size-1.5 bg-primary rounded-full',
-            isSelected && "bg-white")} />
-          )} 
+            <div
+              className={cn(
+                "absolute bottom-3 left-1/2 transform -translate-x-1/2 translate-y-1/2 size-1.5 bg-primary rounded-full",
+                isSelected && "bg-white"
+              )}
+            />
+          )}
+        </div>
       </div>
-    </div>
-   </td>
+    </td>
   );
 }
