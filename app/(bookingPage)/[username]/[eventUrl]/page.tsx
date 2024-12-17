b03b69bf01ccd4861c9952d380d06fa8c8bb5812
0x1Shub <shubhamyeram13@gmail.com>
2024-12-17 20:19:40 +0530

    Implemented Calender Structure

--- app/(bookingPage)/[username]/[eventUrl]/page.tsx
+++ app/(bookingPage)/[username]/[eventUrl]/page.tsx
@@ -1,3 +1,5 @@
+import { Calendar } from "@/app/components/bookingForm/Calendar";
+import { RenderCalendar } from "@/app/components/bookingForm/RenderCalendar";
 import prisma from "@/app/lib/db";
 import { Card, CardContent } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
@@ -53,8 +55,8 @@ export default async function BookingFormRoute({
 
     return (
         <div className="min-h-screen w-screen flex items-center justify-center">
-            <Card className="max-w-[1000px] w-full mx-auto">
-                <CardContent className="p-5 md:grid md:grid-cols-[1fr, auto, 1fr, auto, 1fr]">
+            <Card className="max-w-[1000px] w-full mx-auto">    
+                <CardContent className="p-5 md:grid md:grid-cols-[1fr, auto, 1fr, auto, 1fr] gap-4">  
                     <div>
                         <img src={data.User?.image as string} alt="Profile Image of user" className="size-10 rounded-full" />
                         <p className="text-sm font-medium text-muted-foreground mt-1">{data.User?.name}</p>
@@ -87,6 +89,7 @@ export default async function BookingFormRoute({
 
                     <Separator orientation="vertical" className="h-full w-[1px]" />
 
+                    <RenderCalendar availability={data.User?.availability as any} />
                 </CardContent>
             </Card>
         </div>
