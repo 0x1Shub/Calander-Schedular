7afe85c30dd19397fc4ae368c9cbd58ef4974936
0x1Shub <shubhamyeram13@gmail.com>
2024-12-19 19:42:56 +0530

    Implemented Timetable

--- app/(bookingPage)/[username]/[eventUrl]/page.tsx
+++ app/(bookingPage)/[username]/[eventUrl]/page.tsx
@@ -1,5 +1,6 @@
 import { Calendar } from "@/app/components/bookingForm/Calendar";
 import { RenderCalendar } from "@/app/components/bookingForm/RenderCalendar";
+import { TimeTable } from "@/app/components/bookingForm/TimeTable";
 import prisma from "@/app/lib/db";
 import { Card, CardContent } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
@@ -60,47 +61,113 @@ export default async function BookingFormRoute({
         month: "long",
     }).format(selectedDate);
 
+    const showForm = !!searchParams.date && !!searchParams.time;
+
     return (
         <div className="min-h-screen w-screen flex items-center justify-center">
-            <Card className="max-w-[1000px] w-full">    
-                <CardContent className="p-5 md:grid md:grid-cols-[1fr,auto,1fr] gap-4">  
-                    <div>
-                        <img src={data.User?.image as string} alt="Profile Image of user" className="size-10 rounded-full" />
-                        <p className="text-sm font-medium text-muted-foreground mt-1">{data.User?.name}</p>
+            {showForm ? (
+                <Card className="max-w-[600px]">
+                <CardContent className="p-5 md:grid md:grid-cols-[1fr,auto,1fr] gap-4">
+                  <div>
+                    <img
+                      src={data.User?.image as string}
+                      alt="Profile Image of user"
+                      className="size-10 rounded-full"
+                    />
+                    <p className="text-sm font-medium text-muted-foreground mt-1">
+                      {data.User?.name}
+                    </p>
+                    <h1 className="text-xl font-semibold mt-2">{data.title}</h1>
+                    <p className="text-sm font-medium text-muted-foreground">
+                      {data.description}
+                    </p>
+      
+                    <div className="mt-5 flex flex-col gap-y-3">
+                      <p className="flex items-center">
+                        <CalendarX2 className="size-4 mr-2 text-primary" />
+                        <span className="text-sm font-medium text-muted-foreground">
+                          {formattedDate}
+                        </span>
+                      </p>
+      
+                      <p className="flex items-center">
+                        <Clock className="size-4 mr-2 text-primary" />
+                        <span className="text-sm font-medium text-muted-foreground">
+                          {data.duration} Minutes
+                        </span>
+                      </p>
+      
+                      <p className="flex items-center">
+                        <VideoIcon className="size-4 mr-2 text-primary" />
+                        <span className="text-sm font-medium text-muted-foreground">
+                          {data.videoCallSoftware}
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+      
+                  <Separator orientation="vertical" className="h-full w-[1px]" />
+
+                  <form>
+                    <p>form</p>
+                  </form>
+                </CardContent>
+              </Card>
+            ): (
+                <Card className="w-full max-w-[1000px] mx-auto">
+                    <CardContent className="p-5 md:grid md:grid-cols-[1fr,auto,1fr,auto,1fr] gap-4">
+                        <div>
+                        <img
+                            src={data.User?.image as string}
+                            alt="Profile Image of user"
+                            className="size-10 rounded-full"
+                        />
+                        <p className="text-sm font-medium text-muted-foreground mt-1">
+                            {data.User?.name}
+                        </p>
                         <h1 className="text-xl font-semibold mt-2">{data.title}</h1>
-                        <p className="text-sm font-medium text-muted-foreground">{data.description}</p>
+                        <p className="text-sm font-medium text-muted-foreground">
+                            {data.description}
+                        </p>
 
                         <div className="mt-5 flex flex-col gap-y-3">
                             <p className="flex items-center">
-                                <CalendarX2 className="size-4 mr-2 text-primary" />
-                                <span className="text-sm font-medium text-muted-foreground">
-                                    {formattedDate}
-                                </span>
+                            <CalendarX2 className="size-4 mr-2 text-primary" />
+                            <span className="text-sm font-medium text-muted-foreground">
+                                {formattedDate}
+                            </span>
                             </p>
 
                             <p className="flex items-center">
-                                <Clock className="size-4 mr-2 text-primary" />
-                                <span className="text-sm font-medium text-muted-foreground">
-                                    {data.duration} Minutes
-                                </span>
+                            <Clock className="size-4 mr-2 text-primary" />
+                            <span className="text-sm font-medium text-muted-foreground">
+                                {data.duration} Minutes
+                            </span>
                             </p>
 
                             <p className="flex items-center">
-                                <VideoIcon className="size-4 mr-2 text-primary" />
-                                <span className="text-sm font-medium text-muted-foreground">
-                                    {data.videoCallSoftware} 
-                                </span>
+                            <VideoIcon className="size-4 mr-2 text-primary" />
+                            <span className="text-sm font-medium text-muted-foreground">
+                                {data.videoCallSoftware}
+                            </span>
                             </p>
                         </div>
-                    </div>
+                        </div>
 
-                    <Separator orientation="vertical" className="h-full w-[1px]" />
+                        <Separator orientation="vertical" className="h-full w-[1px]" />
 
-                    <RenderCalendar availability={data.User?.availability as any} />
+                        <RenderCalendar availability={data.User?.availability as any} />
 
-                    <Separator orientation="vertical" className="h-full w-[1px]" />
-                </CardContent> 
-            </Card>
+                        <Separator orientation="vertical" className="h-full w-[1px]" />
+
+                        <TimeTable
+                        duration={data.duration}
+                        selectedDate={selectedDate}
+                        userName={params.username}
+                        />
+                    </CardContent>
+                </Card>
+            )}
         </div>
     )
 }
