202b7590a1b7e2ffdfc8a5f3211858c6fd64fd3d
0x1Shub <shubhamyeram13@gmail.com>
2024-12-21 23:18:21 +0530

    Implemented Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Navbar } from "./components/Navbar";
 import { auth } from "./lib/auth";
 import { redirect } from "next/navigation";
+import { Hero } from "./components/Hero";
 
 export default async function Home() {
 
@@ -14,6 +15,7 @@ export default async function Home() {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <Navbar />
+      <Hero />
     </div>
   );
 }
