7c1f0e99a117991e5d5b0b81e043f586cd58355c
0x1Shub <shubhamyeram13@gmail.com>
2024-12-21 12:49:25 +0530

    Implemeted Delete Route

--- app/actions.ts
+++ app/actions.ts
@@ -325,3 +325,16 @@ export async function UpdateEventTypeStatusAction(prevState: any, {
     };
   }
 }
+
+export async function DeleteEventTypeAction(formData: FormData) {
+  const session = await requireUser();
+
+  const data = await prisma.eventType.delete({
+    where: {
+      id: formData.get("id") as string,
+      userId: session.user?.id,
+    },
+  });
+
+  return redirect("/dashboard");
+}
